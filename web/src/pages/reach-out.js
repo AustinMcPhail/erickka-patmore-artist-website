23b24b383fa06fc06711d8d671133623c6208a95
Austin McPhail <austin@mcphail.dev>
2020-08-15 00:43:08 -0600

    style: 💄 change reachout layout

--- web/src/pages/reach-out.js
+++ web/src/pages/reach-out.js
@@ -74,8 +74,12 @@ const ReachOutPage = (props) => {
     display: grid;
     grid-gap: 0.5em;
     grid-template-areas:
-      'i f t'
-      'c c c';
+      'c i'
+      'c f'
+      'c t'
+      'c t'
+      'c t'
+      'c t';
   `
 
   const Social = styled.section`
@@ -133,7 +137,30 @@ const ReachOutPage = (props) => {
               Tweets by Erickkapatmore
             </a>
           </TwitterWrapper>
-          <ContactFormWrapper>Contact</ContactFormWrapper>
+          <ContactFormWrapper>
+            {/* <form method='post' netlify-honeypot='bot-field' data-netlify='true' name='contact'>
+              <input type='hidden' name='bot-field' />
+              <input type='hidden' name='form-name' value='contact' />
+              <label>
+                Name
+                <input type='text' name='name' id='name' />
+              </label>
+              <label>
+                Email
+                <input type='email' name='email' id='email' />
+              </label>
+              <label>
+                Subject
+                <input type='text' name='subject' id='subject' />
+              </label>
+              <label>
+                Message
+                <textarea name='message' id='message' rows='5' />
+              </label>
+              <button type='submit'>Send</button>
+              <input type='reset' value='Clear' />
+            </form> */}
+          </ContactFormWrapper>
         </ReachOutWrapper>
       </Layout>
     </ThemeProvider>
