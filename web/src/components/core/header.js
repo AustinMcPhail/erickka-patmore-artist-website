1f571f1a3b76e97d0425bf9ecf68f75cbdeee043
Austin McPhail <austin@vivvo.com>
2020-08-29 21:54:54 -0600

    Revert "Header changes"

--- web/src/components/core/header.js
+++ web/src/components/core/header.js
@@ -1,8 +1,43 @@
 import {Link} from 'gatsby'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
 import FacebookIcon from '../icon/FacebookIcon'
 import InstagramIcon from '../icon/InstagramIcon'
+
+const HeaderWrapper = styled.header`
+  top: 0;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  z-index: 1;
+  margin-bottom: 1em;
+`
+
+const NavList = styled.ul`
+  display: flex;
+  flex-direction: row;
+
+  flex-direction: column-reverse;
+  padding-left: 2em;
+
+  flex-wrap: wrap;
+  justify-content: space-between;
+  font-family: ${(props) => props.theme.secondaryFont};
+
+  text-align: end;
+
+  @media (min-width: 1280px) {
+    flex-direction: row;
+    padding-left: 2em;
+  }
+`
+const NavListItem = styled.li`
+  padding-left: 1em;
+  padding-bottom: 1em;
+  @media (min-width: 1280px) {
+    padding-right: 1em;
+    padding-top: 1em;
+  }
+`
 
 const Brand = styled.div`
   padding-top: 1em;
@@ -15,153 +50,102 @@ const BrandLinks = styled.div`
   align-items: center;
   margin-top: 1em;
 
+  @keyframes zoomTiltIn {
+    from {
+      transform: rotate(0deg) scale(1);
+    }
+    to {
+      transform: rotate(5deg) scale(1.05);
+    }
+  }
+  @keyframes zoomTiltOut {
+    from {
+      transform: rotate(5deg) scale(1.05);
+    }
+    to {
+      transform: rotate(0deg) scale(1);
+    }
+  }
+
   a {
     margin-right: 0.75em;
-    transition: transform 0.25s ease-in-out;
-
-    :hover, :focus {
-      transform: rotate(5deg) scale(1.05)
+    animation: zoomTiltOut 0.25s linear forwards;
+    :hover {
+      animation: zoomTiltIn 0.25s linear forwards;
     }
   }
 `
-const NewHeaderWrapper = styled.header`
-    display: grid;
-    gap: 2rem;
-    grid-template-columns: 1fr 3fr;
 
-    .brand {
+const NavLinkList = styled.ul`
+  left: 0;
+  display: inline-flex;
+  flex-direction: column;
+  a {
+    padding-left: 1em;
+  }
+  @media (min-width: 1280px) {
+    padding-top: 1em;
+    a {
+      padding-right: 1em;
     }
+    flex-direction: row;
+    position: absolute;
+  }
+`
 
-    .nav {
-      .nav-list {
-        padding-top: 1rem;
-        display: flex;
-        justify-content: space-between;
-
-        @media (max-width: 1024px) {
-          flex-direction: column-reverse;
-          margin-bottom: 1rem;
-        }
-        
-        .nav-list-item {
-          position: relative;
-          width: 100%;
-          margin: unset 1rem unset 1rem;
-          text-align: center;
-          @media (max-width: 1024px) {
-              text-align: right;
-              margin:  0.5rem unset 0.5rem unset;
-              padding-top: 1rem;
-              padding-bottom: 1rem;
-          }
-
-          &.nav-list-dropdown {
-            .dropdown-content {
-              margin-top: 0.5rem;
-              display: none;
-              .nav-link.dropdown-item {
-                margin-top: 0.25rem;
-                border-bottom: 2px solid hsl(${(props) => props.theme.backgroundHsl.h}, ${(props) => props.theme.backgroundHsl.s * 100 + '%'}, ${(props) => props.theme.backgroundHsl.l * 100 + '%'}, 1);
-                transition: border-bottom 0.25s ease-in-out;
-                &:hover, :focus {
-                  border-bottom: 2px solid ${(props) => props.theme.fontColor};
-                }
-
-                &.active {
-                  border-bottom: 2px solid ${(props) => props.theme.fontColor};
-                }
-              }
-            }
-
-            &:hover, :focus-within {
-              .dropdown-content {
-                display: flex;
-                flex-direction: column;
-              }
-            }
-
-          }
-
-          .nav-link {
-            @media (max-width: 1024px) {
-              margin-right: 1rem;
-            }
-
-            &:not(.dropdown-item)::after {
-              content: ' ';
-              position: absolute;
-              top: -1rem;
-              left: 0;
-              width: 100%;
-              border-top: 4px solid ${(props) => props.theme.fontColor};
-              @media (max-width: 1024px) {
-                width: unset;
-                border-top: unset;
-                top: 0;
-                left: unset;
-                right: 0;
-                height: 100%;
-                border-right: 4px solid ${(props) => props.theme.fontColor};
-              }
-              opacity: 0.75;
-              transition: clip-path 0.2s ease-in-out;
-              clip-path: circle(0%);
-            }
-
-            &:not(.dropdown-item)::before {
-              content: ' ';
-              position: absolute;
-              width: 100%;
-              height: 4rem;
-              top: -1rem;
-              left: 0;
-              z-index: -1;
-              transition: box-shadow 0.21s ease-in-out;
-              box-shadow: inset 0 0 5px -5px hsl(${(props) => props.theme.backgroundHsl.h}, ${(props) => props.theme.backgroundHsl.s * 100 + '%'}, ${(props) => props.theme.backgroundHsl.l * 100 * 0.5 + '%'}, 0);
-
-              @media (max-width: 1024px) {
-                width: unset;
-                height: 100%;
-                top: 0;
-                right: 0;
-                left: unset;
-                box-shadow: inset 0 0 5px -5px hsl(${(props) => props.theme.backgroundHsl.h}, ${(props) => props.theme.backgroundHsl.s * 100 + '%'}, ${(props) => props.theme.backgroundHsl.l * 100 * 0.5 + '%'}, 0);
-              }
-            }
-            &:hover:not(.active), :focus:not(.active) {
-              &::after {
-                clip-path: circle(100%);
-                @media (max-width: 1024px) {
-                  clip-path: circle(50%);
-                }
-              }
-              &::before {
-                box-shadow: inset 0 -20px 10px -5px hsl(${(props) => props.theme.backgroundHsl.h}, ${(props) => props.theme.backgroundHsl.s * 100 + '%'}, ${(props) => props.theme.backgroundHsl.l * 100 + '%'}, 1), inset 0 10px 20px -5px hsl(${(props) => props.theme.backgroundHsl.h}, ${(props) => props.theme.backgroundHsl.s * 100 + '%'}, ${(props) => props.theme.backgroundHsl.l * 100 * 0.5 + '%'}, 1);
-              }
-            }
-          }
-          
-          .active {
-            &:not(.dropdown-item)::after {
-              clip-path: circle(100%);
-            }
-            &:not(.dropdown-item)::before {
-              box-shadow: inset 0 -20px 10px -5px hsl(${(props) => props.theme.backgroundHsl.h}, ${(props) => props.theme.backgroundHsl.s * 100 + '%'}, ${(props) => props.theme.backgroundHsl.l * 100 + '%'}, 1), inset 0 10px 20px -5px hsl(${(props) => props.theme.backgroundHsl.h}, ${(props) => props.theme.backgroundHsl.s * 100 + '%'}, ${(props) => props.theme.backgroundHsl.l * 100 * 0.5 + '%'}, 1);
-            }
-          }
-        }
-      }
-    }
-  `
+const PortfolioItem = styled.div`
+  position: relative;
+`
 
 const reachOutPath = '/reach-out'
 const cvPath = '/cv'
 const statementPath = '/statement'
 const portfolioPath = '/portfolio'
 
+const inactiveStyle = {
+  borderTop: 'solid 4px transparent',
+  paddingTop: '0.5em'
+}
+
+const subInactiveStyle = {
+  borderTop: 'solid 2px transparent'
+}
+
 const Header = ({siteTitle, categories, socials, fontColor}) => {
+  const [portfolioListOpen, setPortfolioListOpen] = useState(false)
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      setPortfolioListOpen(false)
+    } else if (window.location.href.includes(portfolioPath)) {
+      setPortfolioListOpen(true)
+    }
+  })
+
+  const activeStyle = {
+    borderTop: `solid 4px ${
+      fontColor
+        ? `rgba(${fontColor.rgb.r}, ${fontColor.rgb.g}, ${fontColor.rgb.b}, ${fontColor.rgb.a})`
+        : '#2f2f2f'
+    }`,
+    paddingTop: '0.5em',
+    fontWeight: 'bold',
+    boxShadow: `inset 0 7px 9px -7px ${
+      fontColor
+        ? `rgba(${fontColor.rgb.r}, ${fontColor.rgb.g}, ${fontColor.rgb.b}, 0.4)`
+        : '#2f2f2f'
+    }`
+  }
+
+  const subActiveStyle = {
+    borderTop: `solid 2px ${
+      fontColor
+        ? `rgba(${fontColor.rgb.r}, ${fontColor.rgb.g}, ${fontColor.rgb.b}, ${fontColor.rgb.a})`
+        : '#2f2f2f'
+    }`
+  }
+
   return (
-    <NewHeaderWrapper>
+    <HeaderWrapper>
       <Brand>
         <Link to='/'>
           {siteTitle &&
@@ -176,38 +160,40 @@ const Header = ({siteTitle, categories, socials, fontColor}) => {
           {socials && socials.facebookUrl && <FacebookIcon url={socials.facebookUrl} />}
         </BrandLinks>
       </Brand>
-      <nav className='nav'>
-        <ul className='nav-list'>
-          <li className='nav-list-item nav-list-dropdown'>
-            <Link className='nav-link' activeClassName='active' to={portfolioPath} partiallyActive>
-              Portfolio
+      <nav>
+        <NavList>
+          <PortfolioItem style={portfolioListOpen ? activeStyle : inactiveStyle}>
+            <Link to={portfolioPath}>
+              <NavListItem>Portfolio</NavListItem>
             </Link>
-            <div className='dropdown-content'>
-              {categories.map((cat, i) => (
-                <Link className='nav-link dropdown-item' activeClassName='active' to={'/portfolio/' + cat.slug.current} key={'category.' + cat.slug.current}>
-                  {cat.title}
-                </Link>
-              ))}
-            </div>
-          </li>
-          <li className='nav-list-item'>
-            <Link className='nav-link' activeClassName='active' to={statementPath}>
-              Statement
-            </Link>
-          </li>
-          <li className='nav-list-item'>
-            <Link className='nav-link' activeClassName='active' to={reachOutPath}>
-              Reach Out
-            </Link>
-          </li>
-          <li className='nav-list-item'>
-            <Link className='nav-link' activeClassName='active' to={cvPath}>
-              CV
-            </Link>
-          </li>
-        </ul>
+            {portfolioListOpen && (
+              <NavLinkList>
+                {categories.map((cat, i) => (
+                  <Link
+                    className={'test'}
+                    key={'category.' + cat.slug.current}
+                    to={'/portfolio/' + cat.slug.current}
+                    activeStyle={subActiveStyle}
+                    style={{paddingBottom: '.5em', ...subInactiveStyle}}
+                  >
+                    <li>{cat.title}</li>
+                  </Link>
+                ))}
+              </NavLinkList>
+            )}
+          </PortfolioItem>
+          <Link to={reachOutPath} activeStyle={activeStyle} style={inactiveStyle} partiallyActive>
+            <NavListItem>Reach Out</NavListItem>
+          </Link>
+          <Link to={statementPath} activeStyle={activeStyle} style={inactiveStyle} partiallyActive>
+            <NavListItem>Statement</NavListItem>
+          </Link>
+          <Link to={cvPath} activeStyle={activeStyle} style={inactiveStyle} partiallyActive>
+            <NavListItem>CV</NavListItem>
+          </Link>
+        </NavList>
       </nav>
-    </NewHeaderWrapper>
+    </HeaderWrapper>
   )
 }
 
--- web/src/pages/statement.js
+++ web/src/pages/statement.js
@@ -9,27 +9,6 @@ import {imageUrlFor} from '../lib/image-url'
 import {buildImageObj} from '../lib/helpers'
 
 export const query = graphql`
-fragment SanityMImage on SanityMainImage {
-  crop {
-    _key
-    _type
-    top
-    bottom
-    left
-    right
-  }
-  hotspot {
-    _key
-    _type
-    x
-    y
-    height
-    width
-  }
-  asset {
-    _id
-  }
-}
   query StatementPageQuery {
     site: sanitySiteSettings(_id: {regex: "/(drafts.|)siteSettings/"}) {
       title
@@ -61,8 +40,28 @@ fragment SanityMImage on SanityMainImage {
       }
       _rawStatement(resolveReferences: {maxDepth: 5})
       statementImage {
-        ...SanityMImage
-            alt
+        crop {
+          _key
+          _type
+          top
+          bottom
+          left
+          right
+        }
+        hotspot {
+          _key
+          _type
+          x
+          y
+          height
+          width
+        }
+        asset {
+          _id
+          fluid(maxWidth: 400) {
+            ...GatsbySanityImageFluid
+          }
+        }
       }
       author {
         image {
@@ -78,72 +77,26 @@ fragment SanityMImage on SanityMainImage {
   }
 `
 
-const StatementWrapper = styled.section`
-  .statement-container {
-    margin-bottom: 4rem;
-    display: grid;
-    align-items: center;
-    gap: 1rem;
-    justify-items: center;
-    grid-template-areas:
-      'R'
-      'L';
-    @media (min-width: 1024px) {
-      grid-template-areas:
-        'L R';
-    }
-    
-    .left {
-      grid-area: L;
-      &.img-container {
-        animation: shadowedFadeInFromBottomLeft 1s ease-in-out forwards;
-      }
-      @keyframes shadowedFadeInFromBottomLeft {
-          0% {
-            transform: translateY(10px) translateX(-10px);
-            opacity: 0;
-            box-shadow: 0px 10px 20px 5px
-            hsl(${(props) => props.theme.backgroundHsl.h}, ${(props) => props.theme.backgroundHsl.s * 100 - props.theme.backgroundHsl.s * 100 * 0.5 + '%'}, ${(props) => props.theme.backgroundHsl.l * 100 - props.theme.backgroundHsl.l * 100 * 0.5 + '%'}, 0);
-          }
-          100% {
-            transform: translateY(0px) translateX(0px);
-            opacity: 1;
-            box-shadow: 0px 15px 10px -10px
-            hsl(${(props) => props.theme.backgroundHsl.h}, ${(props) => props.theme.backgroundHsl.s * 100 - props.theme.backgroundHsl.s * 100 * 0.5 + '%'}, ${(props) => props.theme.backgroundHsl.l * 100 - props.theme.backgroundHsl.l * 100 * 0.5 + '%'}, 1);
-          }
-      }
-    }
+const StatementWrapper = styled.article`
+  display: grid;
+  grid-template-areas:
+    's'
+    'i';
+  gap: 1rem;
 
-    .right {
-      grid-area: R;
-      animation: fadeInFromTopRight 1s ease-in-out forwards;
-      @keyframes fadeInFromTopRight {
-        0% {
-          transform: translateY(-10px) translateX(10px);
-          opacity: 0;
-        }
-        100% {
-          transform: translateY(0px) translateX(0px);
-          opacity: 1;
-        }
-      }
-    }
+  @media (min-width: 1280px) {
+    grid-template-areas: 'i s s s';
+  }
 
-    .statement-body {
-      width: 100%;
-    }
+  #statementImage {
+    grid-area: i;
+  }
 
-    .img-container {
-      display: grid;
-      height: 100%;
-
-      .fit-image {
-        transition: filter 0.25s ease-in-out, transform 0.25s ease-in-out;
-        max-width: 100%;
-        max-height: 75vh;
-        margin: auto;
-      }
-    }
+  #statement {
+    grid-area: s;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
   }
 `
 
@@ -176,19 +129,21 @@ const StatementPage = (props) => {
       <GlobalStyle />
       <Layout site={site} categories={[]} socials={socials}>
         <StatementWrapper>
-          <div className='statement-container'>
-            <div className='img-container left'>
+          {site.statementImage && site.statementImage.asset && (
+            <div id='statementImage'>
               <img
                 src={imageUrlFor(buildImageObj(site.statementImage))
+                  .width(400)
+                  .height(Math.floor((9 / 16) * 1000))
+                  .fit('crop')
                   .auto('format')
                   .url()}
-                alt={site.statementImage.alt}
-                className='fit-image'
+                alt={'Statement Splash Image'}
               />
             </div>
-            <article className={'statement-body right'}>
-              {site._rawStatement && <PortableText blocks={site._rawStatement} />}
-            </article>
+          )}
+          <div id='statement'>
+            {site._rawStatement && <PortableText blocks={site._rawStatement} />}
           </div>
         </StatementWrapper>
       </Layout>
