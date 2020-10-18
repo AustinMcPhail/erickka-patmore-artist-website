fff7061bd007ee1b9840566ae238a0b13935f6fd
Austin McPhail <austin@mcphail.dev>
2020-10-17 23:39:16 -0600

    style: 💄 update styling

--- web/src/lib/styled.js
+++ web/src/lib/styled.js
@@ -6,16 +6,23 @@ export const GlobalStyle = createGlobalStyle`
   figure{margin:0;}
   input::-moz-focus-inner {border:0; padding:0; margin:0;}
   ul, ol, dd{margin:0; padding:0; list-style:none;}
-  h1, h2, h3, h4, h5, h6{margin:0; font-weight:inherit;}
+  h1, h2, h3, h4, h5, h6{margin:0;}
   p{margin:0;}
   cite {font-style:normal;}
   fieldset{border-width:0; padding:0; margin:0;}
+
+  html, body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
 
   body {
     background-color: ${(props) => props.theme.backgroundColor};
     font-family: 'Montserrat', sans-serif;
     color: ${(props) => props.theme.fontColor};
+    font-size: 16px;
   }
 `
 
