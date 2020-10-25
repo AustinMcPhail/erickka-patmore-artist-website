60cf13ab1d30d59e092e3e8436225ed11fa1bcbb
Austin McPhail <austin@mcphail.dev>
2020-10-25 12:36:23 -0600

    fix: 🐛 get locationhref properly

--- web/src/components/JournalPost.js
+++ web/src/components/JournalPost.js
@@ -156,6 +156,7 @@ const IconSvg = styled.svg`
 
 export const JournalPost = ({prev, post, next}) => {
   const {title, mainImage, _rawBody, publishedAt, authors} = post
+  const url = typeof window !== 'undefined' ? window.location.href : ''
   return (
     <Post>
       <div className='journal-nav'>
@@ -188,12 +189,12 @@ export const JournalPost = ({prev, post, next}) => {
         <div className='post-footer'>
           <div className='share'>
             <span>Share with</span>
-            <a href={`https://twitter.com/intent/tweet/?url=${window.location.href}`}target='_blank'>
+            <a href={`https://twitter.com/intent/tweet/?url=${url}`}target='_blank' rel='noopener noreferrer'>
               <IconSvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24px' height='24px'>
                 <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24px' height='24px'><path d='M24,4.3c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2C19.3,2.6,18,2,16.6,2 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,7.8,4.1,5.9,1.7,2.9C1.2,3.6,1,4.5,1,5.4c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.4,1.6,9.2,1,8.9c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.2,23.3,5.3,24,4.3' /></svg>
               </IconSvg>
             </a>
-            <a href={`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`}target='_blank'>
+            <a href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}target='_blank' rel='noopener noreferrer'>
               <IconSvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24px' height='24px'>
                 <path d='M 21.800781 0 L 2.199219 0 C 1 0 0 1 0 2.199219 L 0 21.800781 C 0 23 1 24 2.199219 24 L 12 24 L 12 14 L 9 14 L 9 11 L 12 11 L 12 8 C 12 5.5 13 4 16 4 L 19 4 L 19 7 L 17.699219 7 C 16.800781 7 16 7.800781 16 8.699219 L 16 11 L 20 11 L 19.5 14 L 16 14 L 16 24 L 21.800781 24 C 23 24 24 23 24 21.800781 L 24 2.199219 C 24 1 23 0 21.800781 0 Z' />
               </IconSvg>
