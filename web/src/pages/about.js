8a7f493cd477d4c8b86423a37c3a5a108bfdbd4c
Austin McPhail <austin@mcphail.dev>
2021-02-13 18:11:38 -0600

    fix: 🐛 remove old stuff

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -1,53 +1,6 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 import styled from 'styled-components'
-import GraphQLErrorList from '../components/graphql-error-list'
-import Layout from '../components/core/layout'
 import Instagram from '../components/Instagram'
-
-export const query = graphql`
-  query ReachOutPageQuery {
-    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
-      title
-      description
-      keywords
-      facebookUrl
-      instagramUrl
-      backgroundColor {
-        rgb {
-          r
-          g
-          b
-          a
-        }
-        hsl {
-          h
-          s
-          l
-          a
-        }
-      }
-      fontColor {
-        rgb {
-          r
-          g
-          b
-          a
-        }
-      }
-    }
-    categories: allSanityCategory(filter: { enabled: { ne: false } }) {
-      edges {
-        node {
-          title
-          slug {
-            current
-          }
-        }
-      }
-    }
-  }
-`
 
 const ReachOutWrapper = styled.div`
   display: flex;
@@ -82,19 +35,12 @@ const ReachOutWrapper = styled.div`
     0% {
       transform: translateY(-10px);
       opacity: 0;
-      box-shadow: 0px 10px 20px 10px
-        hsl(
-          ${(props) => props.theme.backgroundHsl.h},
-          ${(props) =>
-            `${props.theme.backgroundHsl.s * 100 - props.theme.backgroundHsl.s * 100 * 0.5}%`},
-          ${(props) =>
-            `${props.theme.backgroundHsl.l * 100 - props.theme.backgroundHsl.l * 100 * 0.5}%`}
-        );
+      box-shadow: 0px 10px 20px 10px black;
     }
     100% {
       transform: translateY(0px);
       opacity: 1;
-      box-shadow: 0px 15px 10px -10px hsl(${(props) => props.theme.backgroundHsl.h}, ${(props) => `${props.theme.backgroundHsl.s * 100 - props.theme.backgroundHsl.s * 100 * 0.5}%`}, ${(props) => `${props.theme.backgroundHsl.l * 100 - props.theme.backgroundHsl.l * 100 * 0.5}%`});
+      box-shadow: 0px 15px 10px -10px black;
     }
   }
 
@@ -116,57 +62,35 @@ const ReachOutWrapper = styled.div`
   }
 `
 
-const ReachOutPage = (props) => {
-  const { data, errors } = props
-  if (errors) {
-    return (
-      <Layout>
-        <GraphQLErrorList errors={errors} />
-      </Layout>
-    )
-  }
-
-  const { site } = data || {}
-  const categories = data.categories.edges.map((e) => e.node) || []
-  if (!site) {
-    throw new Error(
-      'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
-    )
-  }
-
-  const socials = {
-    facebookUrl: site.facebookUrl,
-    instagramUrl: site.instagramUrl,
-  }
-  return (
-    <ReachOutWrapper>
-      <section aria-labelledby="instagram-header">
-        <div className="heading">
-          <hr />
-          <h3 id="instagram-header">Instagram</h3>
-          <hr />
-        </div>
-        <Instagram />
-      </section>
-      <section aria-labelledby="facebook-header">
-        <div className="heading">
-          <hr />
-          <h3 id="facebook-header">Facebook</h3>
-          <hr />
-        </div>
-        <iframe
-          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ferickkasart&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-          width="340"
-          height="500"
-          style={{ border: 'none', overflow: 'hidden' }}
-          scrolling="no"
-          frameBorder="0"
-          allowtransparency="true"
-          allow="encrypted-media"
-        />
-      </section>
-    </ReachOutWrapper>
-  )
-}
+const ReachOutPage = (props) => (
+  <ReachOutWrapper>
+    <section aria-labelledby="instagram-header">
+      <div className="heading">
+        <hr />
+        <h3 id="instagram-header">Instagram</h3>
+        <hr />
+      </div>
+      <Instagram />
+    </section>
+    <section aria-labelledby="facebook-header">
+      <div className="heading">
+        <hr />
+        <h3 id="facebook-header">Facebook</h3>
+        <hr />
+      </div>
+      <iframe
+        title="unique"
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ferickkasart&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        width="340"
+        height="500"
+        style={{ border: 'none', overflow: 'hidden' }}
+        scrolling="no"
+        frameBorder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
+      />
+    </section>
+  </ReachOutWrapper>
+)
 
 export default ReachOutPage
