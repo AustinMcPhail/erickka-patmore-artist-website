fe07fdcd057eec4e9d834c2a6676ff675475032c
Austin McPhail <austin@mcphail.dev>
2021-03-20 14:16:18 -0600

    feat: 🎸 seo stuff

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,13 +1,23 @@
 // Load variables from `.env` as soon as possible
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV || 'development'}`,
+  path: `.env.${process.env.NODE_ENV || 'production'}`,
 })
 
 const clientConfig = require('./client-config')
 
-const isProd = process.env.NODE_ENV === 'production'
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://erickka.art',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env
+const isProd = NETLIFY_ENV === 'production'
+const siteUrl = isProd ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
 
 module.exports = {
+  siteMetadata: {
+    siteUrl,
+  },
   plugins: [
     {
       resolve: 'gatsby-plugin-google-analytics',
@@ -40,5 +50,27 @@ module.exports = {
       },
     },
     'gatsby-plugin-styled-components',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }],
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
+    },
   ],
 }
