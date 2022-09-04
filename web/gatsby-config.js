416881ee10619aa4684a7d03eed64f55171db863
Austin McPhail <austin@mcphail.dev>
2022-09-04 12:52:02 -0600

    fix: 🐛 add back package

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -42,6 +42,15 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        ...clientConfig.sanity,
+        token: process.env.SANITY_READ_TOKEN,
+        watchMode: !isProd,
+        overlayDrafts: !isProd,
+      },
+    },
+    {
       resolve: "gatsby-plugin-sanity-image",
       options: {
         ...clientConfig.sanity,
